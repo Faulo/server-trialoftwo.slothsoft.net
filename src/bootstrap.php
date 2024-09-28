afdb188977b8c59d9569cd0947944528c5784ed2
Daniel Schulz <faulolio@gmail.com>
2024-09-28 22:41:52 +0200

    build: update files

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,15 +1,16 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\Server\TrialOfTwo;
 
 use Slothsoft\Core\ServerEnvironment;
 use Slothsoft\Farah\Dictionary;
 use Slothsoft\Farah\Kernel;
 use Slothsoft\Farah\Module\Module;
 
-@include __DIR__ . '/../../global/slothsoft.core.php';
-@include __DIR__ . '/../../global/slothsoft.core.xslt.php';
-@include __DIR__ . '/../../global/slothsoft.core.dbms.php';
-@include __DIR__ . '/../../global/slothsoft.farah.php';
+// @include __DIR__ . '/../../global/slothsoft.core.php';
+// @include __DIR__ . '/../../global/slothsoft.core.xslt.php';
+// @include __DIR__ . '/../../global/slothsoft.core.dbms.php';
+// @include __DIR__ . '/../../global/slothsoft.farah.php';
 
 $root = dirname(__DIR__);
 
@@ -18,7 +19,7 @@ ServerEnvironment::setCacheDirectory($root . DIRECTORY_SEPARATOR . 'cache');
 ServerEnvironment::setLogDirectory($root . DIRECTORY_SEPARATOR . 'log');
 ServerEnvironment::setDataDirectory($root . DIRECTORY_SEPARATOR . 'data');
 
-Kernel::setCurrentSitemap('farah://oilcatz@trialoftwo.slothsoft.net/sitemap');
+Kernel::setCurrentSitemap('farah://oilcatz/trialoftwo.slothsoft.net/sitemap');
 Kernel::setTrackingEnabled(false);
 Dictionary::setSupportedLanguages('en-us');
 
